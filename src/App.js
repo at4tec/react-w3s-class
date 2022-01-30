cefe52ce874b96ff3b8ce9bd953211009a2da88e
at4tec <at4tec@gmail.com>
2022-01-30 20:24:32 +0200

    try to pass state to props but not success

--- src/App.js
+++ src/App.js
@@ -2,27 +2,34 @@ import React,{Component} from "react";
 
 
 class Car extends Component{
-  state={
+  constructor(props){
+super(props)
+
+ 
+  this.state={
     color:'green',
     brand:'ford',
     year:2000
   }
- changeColor=()=>{ //expresion is medotory
+  
+  this.changeColor=()=>{ //expresion is medotory
   this.setState({color:'silver'})
-}
+} }
 render(){
  
   return <><h1> hi,i am {this.state.color} {this.state.brand} car
   from year : {this.state.year} </h1>
-  <button onMouseOver={this.changeColor} />
+  
   </>
 }
 }
 
 export default class App extends Component{
+
   render(){
     return <>
     <Car/>
+    <button onMouseOver={this.props.changeColor} />
      </>
   }
 } 
