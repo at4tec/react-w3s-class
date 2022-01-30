90be3625d0e2f86485c10176537de2ece44a8607
at4tec <at4tec@gmail.com>
2022-01-30 17:38:03 +0200

    setState

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React,{Component} from "react";
 
 
 class Car extends Component{
-render(){
-  this.state={
+  state={
     color:'green',
     brand:'ford',
     year:2000
   }
-  return <h1> hi,i am {this.state.color} {this.state.brand} car
+ changeColor=()=>{ //expresion is medotory
+  this.setState({color:'silver'})
+}
+render(){
+ 
+  return <><h1> hi,i am {this.state.color} {this.state.brand} car
   from year : {this.state.year} </h1>
+  <button onMouseOver={this.changeColor} />
+  </>
 }
 }
 
