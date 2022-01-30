532bad60417cc3c2bac5d857166e49c0f180adda
at4tec <at4tec@gmail.com>
2022-01-30 15:08:26 +0200

    make new component and refer to the previous

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React,{Component} from "react";
 
 
+class Car extends Component{
+render(){
+  this.state={
+    color:'green',
+    brand:'ford',
+    year:2000
+  }
+  return <h1> hi,i am {this.state.color} {this.state.brand} car
+  from year : {this.state.year} </h1>
+}
+}
+
 export default class App extends Component{
   render(){
-    this.state={
-      color:'green',
-      brand:'ford',
-      year:2000
-    }
-    return <h1>
-      hi,i am {this.state.color} {this.state.brand} car
-      from year : {this.state.year}
-      </h1>
+    return <>
+    <Car/>
+     </>
   }
 } 
