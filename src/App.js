abd31b78a6dc7c6d09e4d5db04a644e8d342597c
at4tec <at4tec@gmail.com>
2022-01-30 14:59:13 +0200

    make state object

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import React,{Component} from "react";
 
 export default class App extends Component{
   render(){
-    this.state={color:'green'}
+    this.state={
+      color:'green',
+      brand:'ford',
+      year:2000
+    }
     return <h1>
-      hi,i am a {this.state.color} car</h1>
+      hi,i am {this.state.color} {this.state.brand} car
+      from year : {this.state.year}
+      </h1>
   }
 } 
