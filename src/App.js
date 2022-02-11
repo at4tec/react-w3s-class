a8ce79ff1e7bd4bc2f7c08471c683a4fff78b46a
at4tec <at4tec@gmail.com>
2022-02-11 20:33:27 +0200

    remove constructor

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import React,{Component} from "react";
 
-
+//child
 class Car extends Component{
-  constructor(props){
-super(props)
 
  
-  this.state={
+ 
+  state={
     color:'green',
     brand:'ford',
-    year:2000
-  }
+    year:2000}
   
-  this.changeColor=()=>{ //expresion is medotory
+  
+  changeColor=()=>{ //expresion is medotory
   this.setState({color:'silver'})
-} }
+} 
 render(){
  
   return <><h1> hi,i am {this.state.color} {this.state.brand} car
   from year : {this.state.year} </h1>
-  
+  <button onMouseOver={this.changeColor} />
   </>
 }
 }
 
+//parent
 export default class App extends Component{
 
   render(){
     return <>
     <Car/>
-    <button onMouseOver={this.props.changeColor} />
+  
      </>
   }
 } 
